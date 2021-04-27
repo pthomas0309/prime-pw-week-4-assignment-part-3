498c45f0cd704fc894e30da695f4a72fff6217b8
pthomas0309 <preston.thomas355@gmail.com>
2021-04-26 20:53:35 -0500

    created basket array and established function to add values to the array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,10 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item) {
+  console.log('Adding an item to my cart', item);
+  basket.push(item);
+  return true;
+}
